866101e08ad903858aa7cf65c5ec2f7b5c19f626
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-08-15 22:19:44 +0530

    bookschema data function is added

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,36 @@
 import express from "express"
 import { PORT, MongodbURL } from "./config.js"
 import mongoose from "mongoose"
+import { book } from "./models/bookmodels.js"
 const app = express()
 
 
-
+app.use(express.json())
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(555).send("Welcome to booksky website");
+})
+
+app.post('/book', async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishedYear) {
+            res.status(201).send({
+                message: "send all required fields"
+            })
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishedYear: req.body.publishedYear,
+        }
+        const Book = await book.create(newBook)
+
+        return res.status(202).send(Book);
+    }
+    catch (error) {
+        console.log(error.message)
+        res.status(200).send({ message: error.message })
+    }
 })
 
 mongoose.connect(MongodbURL)
